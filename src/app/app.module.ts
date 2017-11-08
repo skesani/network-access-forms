7d4b8197419da6d2078b6f53fe7a36ae5308b766
sasikesani <kesanisasikanth@gmail.com>
2017-11-08 17:12:40 -0500

    Adding reactive form example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
 
 
-import { ShowErrorsComponent } from './error-components/show-errors.component';
+import {ShowErrorsComponent} from './error-components/show-errors.component';
 
-import { AppComponent } from './app.component';
-import { NetworkaccessComponent } from './networkaccess/networkaccess.component';
-import { HeaderComponent } from './header/header.component';
-import { UpdatedformComponent } from './updatedform/updatedform.component';
-import { CuurentformComponent } from './cuurentform/cuurentform.component';
+import {AppComponent} from './app.component';
+import {NetworkaccessComponent} from './networkaccess/networkaccess.component';
+import {HeaderComponent} from './header/header.component';
+import {UpdatedformComponent} from './updatedform/updatedform.component';
+import {CuurentformComponent} from './cuurentform/cuurentform.component';
+import {ReactFormSelectComponent} from './react-form-select/react-form-select.component';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
-  imports: [BrowserModule, ReactiveFormsModule, FormsModule, HttpModule],
-  declarations: [AppComponent, ShowErrorsComponent, NetworkaccessComponent, HeaderComponent, UpdatedformComponent, CuurentformComponent],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule, HttpModule,
+    RouterModule.forRoot([
+        { path: 'select', component: ReactFormSelectComponent}
+    ])
+  ],
+  declarations: [AppComponent, ShowErrorsComponent,
+    NetworkaccessComponent, HeaderComponent, UpdatedformComponent, CuurentformComponent, ReactFormSelectComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
